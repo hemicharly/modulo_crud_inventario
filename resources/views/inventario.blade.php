f0bf037b1b99fd1bcfa34b81ac2625295698545d
hemicharly <hemicharlythiago@gmail.com>
2019-07-09 17:58:19 -0300

    crud

--- resources/views/inventario.blade.php
+++ resources/views/inventario.blade.php
@@ -3,39 +3,45 @@
 @section('body')
 <div class="card border">
     <div class="card-body">
-        <h5 class="card-title text-uppercase">Lista de Inventários</h5>
+        <div class="row">
+            <div class="col-md-9 m-auto text-left">
+                <h5 class="card-title text-uppercase">Lista de Inventários</h5>
+            </div>
+            <div class="col-md-3 m-auto text-right">
+                <a href="/inventario/novo" class="card-title btn btn-block btn-success btn-md" role="button">Novo inventário</a>
+            </div>
+        </div>
         @if(count($listaInventarios) > 0)
-        <table class="table table-ordered table-hover">
-            <thead>
-                <tr>
-                    <th class="text-uppercase">Código Prod.</th>
-                    <th class="text-uppercase">Descrição Prod.</th>
-                    <th class="text-uppercase text-center">Qtd. Prod.</th>
-                    <th class="text-uppercase text-center">Preço Prod.</th>
-                    <th class="text-uppercase text-center">Preço Total</th>
-                    <th class="text-uppercase text-center">Ações</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach($listaInventarios as $inv)
-                <tr>
-                    <td>{{$inv->codigoProduto}}</td>
-                    <td class="text-uppercase">{{$inv->descricaoProduto}}</td>
-                    <td class="text-center">{{$inv->qtdeProduto}}</td>
-                    <td class="text-center">{{$inv->precoProduto}}</td>
-                    <td class="text-center">{{$inv->qtdeProduto*$inv->precoProduto}}</td>
-                    <td class="text-center">
-                        <a href="/inventario/editar/{{$inv->id}}" class="btn btn-primary btn-sm">Editar</a>
-                        <a href="/inventario/apagar/{{$inv->id}}" class="btn btn-danger btn-sm">Apagar</a>
-                    </td>
-                </tr>
-                @endforeach
-            </tbody>
-        </table>
+        <div class="table-responsive">
+            <table class="table table-reponsive table-ordered table-md table-hover">
+                <thead class="thead-light">
+                    <tr>
+                        <th class="text-uppercase">Código Prod.</th>
+                        <th class="text-uppercase">Descrição Prod.</th>
+                        <th class="text-uppercase text-center">Qtd. Prod.</th>
+                        <th class="text-uppercase text-center">Preço Prod.</th>
+                        <th class="text-uppercase text-center">Preço Total</th>
+                        <th class="text-uppercase text-right">Ações</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach($listaInventarios as $inv)
+                    <tr>
+                        <td>{{$inv->codigoProduto}}</td>
+                        <td class="text-uppercase">{{$inv->descricaoProduto}}</td>
+                        <td class="text-center">{{$inv->qtdeProduto}}</td>
+                        <td class="text-center">{{$inv->precoProduto}}</td>
+                        <td class="text-center">{{$inv->qtdeProduto*$inv->precoProduto}}</td>
+                        <td class="text-right">
+                            <a href="/inventario/editar/{{$inv->id}}" class="btn btn-primary btn-sm">Editar</a>
+                            <a href="/inventario/apagar/{{$inv->id}}" class="btn btn-danger btn-sm">Apagar</a>
+                        </td>
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
         @endif
-    </div>
-    <div class="card-footer text-center">
-        <a href="/inventario/novo" class="btn btn-primary btn-md" role="button">Novo inventário</a>
     </div>
 </div>
 @endsection
